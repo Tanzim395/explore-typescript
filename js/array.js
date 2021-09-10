d97dda348145aba13a68bedea06500b20f999866
Tanzim395 <junaidulkader@gmail.com>
2021-09-10 21:49:35 +0600

    array corrected

--- js/array.js
+++ js/array.js
@@ -4,10 +4,10 @@ const friendSalaries = [7500, 12300, 17200, 9400, 8300];
 const friends = ['Sakib', 'Rakib', 'Akib', 'Takib'];
 friendSalaries[0] = 10500;
 friendSalaries.push(13100);
-friendSalaries[4] = '9800';
-friendSalaries.push('7500');
-friends.push('Bakib');
-friends[2] = 'Qakib';
+// friendSalaries[4] = '9800';
+// friendSalaries.push('7500');
+// friends.push('Bakib');
+// friends[2] = 'Qakib';
 let max = 0;
 for (const salary of friendSalaries) {
     if (salary > max) {
